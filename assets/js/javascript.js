f4eb7343faab9e323d456d85dea6239603dc1896
Aditya-Sinha-7981 <aditya20005sinha@gmail.com>
2021-12-08 10:59:42 +0530

    Refer to readme for changes

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,4 +1,7 @@
 const navbar = document.querySelector('.navbar');
+const hamburgerButton = document.querySelector('.hamburger-btn');
+const hamburgerIcon = document.querySelector('#icon');
+const hamburgerClass = document.querySelector('.hamburger-menu');
 
 $(navbar).ready(function(){
     $(window).scroll(function(){
@@ -10,3 +13,29 @@ $(navbar).ready(function(){
         }
     })
 })
+
+//Functions
+
+function hamburgerClassLogic(){
+    //Removal or addition of "active" class for hamburger
+
+    let x = hamburgerClass
+    
+    if(x.classList.contains('active')){
+        x.classList.remove('active');
+    }else{
+        x.classList.add('active');
+    }
+
+    let y = hamburgerIcon
+
+    if(y.classList.contains('fa-bars')){
+        y.classList.remove('fa-bars');
+        y.classList.add('fa-times');
+    }else{
+        y.classList.remove('fa-times');
+        y.classList.add('fa-bars');
+    }
+}
+
+hamburgerButton.addEventListener('click', hamburgerClassLogic);
--- readme.txt
+++ readme.txt
@@ -6,10 +6,22 @@ Progress:
 
 Initialized the template along with landing section and information section done
 
-<----------Section:----------> 
+
+<----------Section:---------->
+
 
 Landing page: Desktop, Tablet and Mobile done 
 
 Information section: Desktop, Tablet and Mobile done
 
 More-products: Desktop,Tablet and Mobile done
+
+
+<----------Update 1:----------> 
+
+
+Hamburger has been added to navbar and it's completely responsive, both navbar and hamburger, transition needs to be added
+
+Landing page is now completely responsive along with minor desing changes
+
+Information section and More products both are completely responsive along with minor design changes
