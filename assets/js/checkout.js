69eb6925ed17b8d8d2ee9b276f1df90caa4dcd41
Aditya-Sinha-7981 <aditya20005sinha@gmail.com>
2022-03-03 19:40:26 +0530

    Client side validation completed

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,4 +1,5 @@
 const fields = document.querySelectorAll('.required')
+const textArea = document.querySelectorAll('.text-area')
 const submit = document.querySelector('.submit-button')
 
 
@@ -7,24 +8,38 @@ const submit = document.querySelector('.submit-button')
 
 submit.addEventListener('click', () => {
 
-    // Creating a "p" element to use it later
-
-    let txt = document.createElement('p')
-    txt.textContent = "Please input something"
-    txt.setAttribute('class', 'error_text')
-
     // looping through all input to check for empty value
 
     for(i = 0; i < fields.length; i++){
 
+        //Creating <small> text element
+
+        const element = document.createElement("small")
+        const text = document.createTextNode("Field cannot be empty")
+        element.className = 'error-text'
+        element.appendChild(text)
+
 
         if(fields[i].value === ''){
+
             fields[i].classList.add('error')
+
+            let smallId = textArea[i].querySelector('.error-text')
+
+            if(textArea[i].contains(smallId)){
+                // This field is empty because is the user does not enter anything, instead of stacking <small> text it stops at one
+            }else{
+                textArea[i].appendChild(element)
+            }
         }else if(fields[i].value !== ''){
 
             //Looping to change error fields to normal if value is inputted
             if(fields[i].classList.contains('error')){
                 fields[i].classList.remove('error')
+
+                let smallId = textArea[i].querySelector('.error-text')
+
+                fields[i].parentNode.removeChild(smallId)
             }
         }
     }
--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,7 @@
+const required = document.querySelectorAll('.required')
+const inputArea = document.querySelectorAll('.input-area')
+const submitButton = document.querySelector('#submit-button')
+
 const slider = tns({
     container: ".carousel-connect",
     nav: false,
@@ -14,6 +18,42 @@ const slider = tns({
         },
         769: {
             items: 3
+        }
+    }
+})
+
+
+submitButton.addEventListener('click', () => {
+    for(i = 0; i < required.length; i++){
+
+        const element = document.createElement("small")
+        const text = document.createTextNode("Field cannot be empty")
+        element.className = 'error-text'
+        element.appendChild(text)
+
+        if(required[i].value === ''){
+
+            required[i].classList.add('error')
+
+            let smallId = inputArea[i].querySelector('.error-text')
+
+            if(inputArea[i].contains(smallId)){
+                // This field is empty because is the user does not enter anything, instead of stacking <small> text it stops at one
+            }else{
+                inputArea[i].appendChild(element)
+            }
+
+        }else{
+
+
+            if(required[i].classList.contains('error')){
+                
+                required[i].classList.remove('error')
+
+                let smallId = inputArea[i].querySelector('.error-text')
+
+                required[i].parentNode.removeChild(smallId)
+            }
         }
     }
 })
